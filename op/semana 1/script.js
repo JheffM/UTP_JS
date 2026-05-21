33434288fc732dceb8f561282ba9097dcedc45ca
JheffM <jheffmartinezba@gmail.com>
2026-05-21 18:58:55 -0500

    todo

---
+++ op/semana 1/script.js
@@ -0,0 +1,25 @@
+let resultado = document.getElementById("resultado");
+
+
+let sumaPares = 0;
+let sumaImpares = 0;
+
+for(let i = 1; i <= 10; i++){
+
+    if(i % 2 === 0){
+
+        resultado.innerHTML += `<p>${i} es par</p>`;
+        sumaPares += i;
+
+    }else{
+
+        resultado.innerHTML += `<p>${i} es impar</p>`;
+        sumaImpares += i;
+
+    }
+
+}
+let sumPares = document.getElementById("sumas");
+// Mostrar sumas
+sumPares.innerHTML += `<h3>Suma de pares: ${sumaPares}</h3>`;
+sumPares.innerHTML += `<h3>Suma de impares: ${sumaImpares}</h3>`;
